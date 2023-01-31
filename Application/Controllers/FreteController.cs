bf30036bfa1191e2db1b968094d4aaef74909b32
Rubem de Almeida <rubemdealmeida@hotmail.com>
2023-01-31 17:44:21 -0300

    Finalize Architeture and create Upload XLS endpoint

--- Application/Controllers/FreteController.cs
+++ Application/Controllers/FreteController.cs
@@ -5,6 +5,13 @@ using Domain.Interfaces;
 using Service.Validators;
 using System;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Hosting;
+using ClosedXML.Excel;
+using System.Net.Http.Headers;
+using ExcelDataReader;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.IO;
+using System.Data;
 
 namespace Application.Controllers
 {
@@ -19,40 +26,61 @@ namespace Application.Controllers
             _baseFreteService = baseFreteService;
         }
         [HttpPost]
-        public  async Task<IActionResult> Create(List<IFormFile> files)
+        public async Task<IActionResult> Create(IFormCollection formData)
         {
-            long size = files.Sum(f => f.Length);
-
-            foreach (var formFile in files)
+            var files = HttpContext.Request.Form.Files;
+            List<Archive> archives = new List<Archive>(); 
+            foreach (var file in files)
             {
-                if (formFile.Length > 0)
+                if (file.Length > 0)
                 {
-                    var filePath = Path.GetTempFileName();
-
-                    using (var stream = System.IO.File.Create(filePath))
+                    System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                    using var fileStream = file.OpenReadStream();
+                    // act on the Base64 data
+                    var worksheet = new Worksheet();
+                    using (var reader = ExcelReaderFactory.CreateReader(fileStream))
                     {
-                        await formFile.CopyToAsync(stream);
+
+                        DataSet ds = reader.AsDataSet();
+                        DataTable dt = ds.Tables[0];
+                        foreach (var row in dt.Rows.Cast<DataRow>().Skip(1))
+                        {
+                            Archive archive = new Archive();
+                            if (row.ItemArray.All(x => x.ToString() != ""))
+                            {
+                                archive.Origin = row.ItemArray[0].ToString();
+                                archive.Dellivery = Int32.Parse(row.ItemArray[1].ToString());
+                                archive.TravelNumber = Int32.Parse(row.ItemArray[2].ToString());
+                                archive.Date = DateTime.Parse(row.ItemArray[3].ToString());
+                                archive.Destination = row.ItemArray[4].ToString();
+                                archive.Plate = row.ItemArray[5].ToString();
+                                archive.Driver = row.ItemArray[6].ToString();
+                                archive.VechicleType = row.ItemArray[7].ToString();
+                                archive.Km = Int32.Parse(row.ItemArray[8].ToString());
+                                archive.Boxes = Int32.Parse(row.ItemArray[9].ToString());
+                                archive.TravelType = row.ItemArray[10].ToString();
+                                archives.Add(archive);
+                            }
+                        };
                     }
                 }
-            }
-            Archive archive = new Archive();
-
+        };
             return null;//Execute(() => _baseFreteService.Add<ArchiveValidator>(archive).Id);
         }
 
 
-        private IActionResult Execute(Func<object> func)
+    private IActionResult Execute(Func<object> func)
+    {
+        try
         {
-            try
-            {
-                var result = func();
+            var result = func();
 
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex);
-            }
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex);
         }
     }
+}
 }
--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -8,41 +8,41 @@ namespace Domain.Entities
 {
     public abstract class BaseEntity
     {
-        public virtual int Id { get; set; }
+        public virtual int? Id { get; set; }
 
     }
 
     public abstract class InfoBase : BaseEntity
     {
 
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
 
-        public int TravelNumber { get; set; }
+        public int? TravelNumber { get; set; }
 
-        public string Driver { get; set; }
+        public string? Driver { get; set; }
 
-        public string Plate { get; set; }
+        public string? Plate { get; set; }
 
-        public string VechicleType { get; set; }
+        public string? VechicleType { get; set; }
 
-        public string Origin { get; set; }
+        public string? Origin { get; set; }
 
-        public string Destination { get; set; }
+        public string? Destination { get; set; }
 
-        public int Boxes { get; set; }
+        public int? Boxes { get; set; }
 
-        public string Dellivery { get; set; }
+        public int? Dellivery { get; set; }
 
-        public int Km { get; set; }
+        public int? Km { get; set; }
 
-        public string TravelType { get; set; }
+        public string? TravelType { get; set; }
     }
 
     public class Frete : InfoBase
     {
-        public string FreightTable { get; set; }
+        public string? FreightTable { get; set; }
 
-        public int TravelValue { get; set; }
+        public int? TravelValue { get; set; }
     }
 
     public class Archive : InfoBase
